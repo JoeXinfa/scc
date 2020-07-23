a5c4470d915168069a1922ca32f269a46440baee
Xinfa Joseph Zhu <xinfazhu@gmail.com>
2020-07-23 13:48:56 -0500

    Start with CMake and DFS

---
+++ README.md
@@ -0,0 +1,11 @@
+Strongly Connected Component
+============================
+
+Report
+------
+https://docs.google.com/document/d/1o5aPxMeNxTJT792qnfjOZvwPvap7l3sbrsKOWPRJ9y4/edit
+
+
+Presentation
+------------
+https://docs.google.com/presentation/d/1krCkIIotzifiJTQIENjw5JC_L6jdXSQzFeRVGXyTQNM/edit?usp=sharing
---
+++ scc/scc.cpp
@@ -0,0 +1,37 @@
+﻿// scc.cpp : Defines the entry point for the application.
+//
+
+#include "scc.h"
+#include "dfs.h"
+
+using namespace std;
+
+/*
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
+*/
+
+int main()
+{
+    // Create a graph given in the above diagram 
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    cout << "Following is Depth First Traversal"
+        " (starting from vertex 2) \n";
+    g.DFS(2);
+    cout << endl;
+    cout << "Following is Depth First Traversal"
+        " (starting from vertex 1) \n";
+    g.DFS(1);
+
+    return 0;
+}
